37a422e0d164bee8499ae680f603b956ae5970aa
kitchanismo <kitchanismo@yahoo.com>
2019-01-11 13:52:37 -0800

    d

--- app/Routes/Root.js
+++ app/Routes/Root.js
@@ -1,41 +1,42 @@
 const Route = use('Route')
 
-const root = () => {
-  return {
-    title: 'kitchanime api',
-    description: 'an anime  api for otaku',
-    routes: [
-      {
-        route: 'auth/login',
-        type: 'guest',
-        request: 'post'
-      },
-      {
-        route: 'auth/register',
-        type: 'guest',
-        request: 'post'
-      },
-      {
-        route: 'api/users',
-        type: 'auth',
-        request: 'get'
-      },
-      {
-        route: 'api/users',
-        type: 'auth',
-        request: 'post'
-      },
-      {
-        route: 'api/users/1',
-        type: 'auth',
-        request: 'get'
-      },
-      {
-        route: 'api/users/1',
-        type: 'auth',
-        request: 'put'
-      }
-    ]
-  }
+const root = ({ response }) => {
+  response.send('kitchanime api')
+  // return {
+  //   title: 'kitchanime api',
+  //   description: 'an anime  api for otaku',
+  //   routes: [
+  //     {
+  //       route: 'auth/login',
+  //       type: 'guest',
+  //       request: 'post'
+  //     },
+  //     {
+  //       route: 'auth/register',
+  //       type: 'guest',
+  //       request: 'post'
+  //     },
+  //     {
+  //       route: 'api/users',
+  //       type: 'auth',
+  //       request: 'get'
+  //     },
+  //     {
+  //       route: 'api/users',
+  //       type: 'auth',
+  //       request: 'post'
+  //     },
+  //     {
+  //       route: 'api/users/1',
+  //       type: 'auth',
+  //       request: 'get'
+  //     },
+  //     {
+  //       route: 'api/users/1',
+  //       type: 'auth',
+  //       request: 'put'
+  //     }
+  //   ]
+  // }
 }
 module.exports = root
